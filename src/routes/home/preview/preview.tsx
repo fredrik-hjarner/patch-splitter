5d367218325f0eaf9c0458d8a0e219a2097689d4
fredrik-hjarner <frederick.hjarner@gmail.com>
2018-07-24 20:07:49 +0200

    Can now download patch fro single file :)

--- src/routes/home/preview/preview.tsx
+++ src/routes/home/preview/preview.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
+import { Context, FileType } from 'routes/home/home';
 import { Form, Icon } from 'semantic-ui-react';
+import { downloadZip } from 'utils';
 
 type Props = {
 	data: string,
@@ -10,15 +12,21 @@ export class Preview extends React.Component<Props> {
 		const { data } = this.props;
 
 		return (
-			<div>
-				<h2>Preview result</h2>
-				<Form onSubmit={this.download}>
-					<Form.Button type="submit" primary><Icon name="download"/> as one file</Form.Button>
-					<Form.Button type="submit" primary><Icon name="download"/> as separate files (in zip)</Form.Button>
-					<Form.Button type="submit" color="green">Validate</Form.Button>
-				</Form>
-				{this.renderText(data)}
-			</div>
+			<Context.Consumer>
+				{({ workInProgress }) => (
+					<div>
+						<h2>Preview result</h2>
+						<Form>
+							<Form.Button disabled primary><Icon name="download"/> as one file</Form.Button>
+							<Form.Button primary onClick={() => this.downloadZip(workInProgress)}>
+								<Icon name="download"/> as separate files (in zip)
+							</Form.Button>
+							<Form.Button disabled color="green">Validate</Form.Button>
+						</Form>
+						{this.renderText(data)}
+					</div>
+				)}
+			</Context.Consumer>
 		);
 	}
 
@@ -29,5 +37,24 @@ export class Preview extends React.Component<Props> {
 		padding: 0,
 	})
 
-	private download = () => {};
+	private downloadZip = (workInProgress: FileType[]) => {
+		const files = workInProgress
+			.filter(file => file.enabled)
+			.map(file => file.text)
+			.filter(text => { // Well... stupid way to remove the patch header
+				if (text.startsWith('diff')) { return true; }
+				console.log('Warning: File did not start with "diff".'); // tslint:disable-line
+				return false;
+			});
+
+		// TODO: this is duplicated in output/actions.tsx
+		const data = files.map(text => {
+			const firstLine = text.split('\n', 1)[0];
+			const path = firstLine.split(' ')[2];
+			const filename = path.substring(2).replace(/\//g, '_');
+			return { filename, text };
+		});
+
+		downloadZip(data, 'patch-files');
+	}
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
 export { lf } from 'utils/lf';
 export { splitPerFile } from 'utils/split-per-file';
 export { randomBgColor } from 'utils/random-bg-color';
-export { downloadText } from './download';
+export { downloadText, downloadZip } from './download';
